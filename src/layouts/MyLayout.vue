86ab8dd240f236805a9596316eb313005b83350a
Dan Shields <danwshields@gmail.com>
2018-12-01 17:06:03 -0700

    layout rm quazar asset

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh Lpr lFf">
     <q-layout-header>
       <q-toolbar
         color="primary"
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/decentralizeco_300dpi_trans.png">
+    <img alt="Quasar logo" height="99%" src="~assets/decentralizeco_300dpi_trans.png">
   </q-page>
 </template>
 
