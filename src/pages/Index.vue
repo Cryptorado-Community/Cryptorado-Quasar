2abb5ac4698203d99f9a0ccb4e36bb5ef8de6c55
Dan Shields <danwshields@gmail.com>
2018-12-13 18:08:19 -0700

    index update with colums and flex used

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,23 @@
 <template>
   <q-page padding>
-    <img alt="co love logo" width="100%" src="~/assets/Colorado-Love-Flag-1350-x-900.png">
+
+    <div class="row items-center justify-center">
+      <div class="col-4">
+        <h3 class="text-weight-bold small"> <em>Building a <br> Blockchain <br> Community</em> </h3>
+      </div>
+      <div class="col-8">
+        <strong><em>Colorado</em></strong> has one of the largest and most active <strong><em>blockchain</em></strong> communities in the world. The state is home to thought leaders and builders of these disruptive technologies. If you want to get connected with fellow entrepreneurs, investors, and developers; <strong><em>Colorado is the best place to be.</em></strong>
+        <br><br>
+        We welcome everyone from the experts to the complete novice!
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+            <img class="shadow-24" alt="co love logo" width="100%" src="~/assets/Colorado-Love-Flag-1350-x-900.png">
+      </div>
+    </div>
+
   </q-page>
 </template>
 
