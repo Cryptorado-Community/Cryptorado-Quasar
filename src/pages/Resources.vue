8d777abe98c3b0f2013a0364dd47a82074eb82a6
Dan Shields <35669742+NukeManDan@users.noreply.github.com>
2019-10-26 22:09:16 -0600

    Update Resources.vue

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -1,41 +1,37 @@
 <template>
   <q-page padding>
-    <h1> <center>!! UNDER CONSTRUCTION !!</center> </h1>
+    <h3> <center>!! UNDER CONSTRUCTION !!</center> </h3>
     <q-list
       no-border
       link
       inset-delimiter
     >
 
-      <q-item @click.native="openURL('http://launchpass.com/coloradoblockchain')">
+      <q-item @click.native="openURL('https://www.meetup.com/Denver-Blockchain/')">
         <q-item-side icon="people" />
         <q-item-main label="Meetups" sublabel="Listing by our Community" />
       </q-item>
 
       <q-collapsible
               icon="record_voice_over"
-              label="Talk"
+              label="Chat"
             >
+            <q-item @click.native="openURL('https://keybase.io/team/cryptorado')">
+              <q-item-side icon="keybase" />
+              <q-item-main label="Keybase" />
+            </q-item>
             <q-item @click.native="openURL('http://launchpass.com/coloradoblockchain')">
               <q-item-side icon="chat_bubble_outline" />
               <q-item-main label="Slack" />
             </q-item>
-            <q-item @click.native="openURL('http://launchpass.com/coloradoblockchain')">
-              <q-item-side icon="chat_bubble_outline" />
-              <q-item-main label="Riot Chat" />
-            </q-item>
       </q-collapsible>
 
-      <q-item @click.native="openURL('http://launchpass.com/coloradoblockchain')">
-        <q-item-side icon="forum" />
-        <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-      </q-item>
       <q-item @click.native="openURL('https://www.youtube.com/channel/UCySU1TwgrLAU_1tF8lDt9bQ')">
-        <q-item-side icon="video_library" />
+        <q-item-side icon="youtube" />
         <q-item-main label="Media" sublabel="YouTube" />
       </q-item>
-      <q-item @click.native="openURL('https://twitter.com/coloblockchain')">
-        <q-item-side icon="rss feed" />
+      <q-item @click.native="openURL('https://twitter.com/cryptoradocom')">
+        <q-item-side icon="twitter" />
         <q-item-main label="Twitter" sublabel="@coloblockchain" />
       </q-item>
     </q-list>
@@ -47,7 +43,7 @@
             <q-item-side icon="map" />
             <q-item-main label="Local Cryptocurrency Accepting Businesses" sublabel="Coinmap.org" />
           </q-item>
-          <q-item @click.native="openURL('https://docs.google.com/spreadsheets/d/1Bvpzr162eWFRo_f3tuhOnCVF-l5USTSBdqeJGVBqjjs/edit#gid=0')">
+          <q-item @click.native="openURL('https://drive.google.com/open?id=10co5YUiYOzyEMhQez5e1ehD6vgb1oOlaSBHzT0gzLQ0')">
             <q-item-side icon="list_alt" />
             <q-item-main label="Colorado Blockchain Companies" />
           </q-item>
