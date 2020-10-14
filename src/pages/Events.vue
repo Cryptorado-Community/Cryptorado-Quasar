0f924ae9cd45d0cb719131c971c84a84de831e67
Dan Shields <nukemandan@protonmail.com>
2020-10-14 14:16:22 -0600

    fix iframe

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -17,8 +17,6 @@
           </div>
       </q-card>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FDenver&amp;" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
   </q-page>
 </template>
 
